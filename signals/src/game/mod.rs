5bd3a80e2549eb93a13c3c03caa9562643002c3f
manen <49951816+manen@users.noreply.github.com>
2025-01-23 14:28:03 +0100

    fix: ingameworlds regenerate on world switch again

--- signals/src/game/mod.rs
+++ signals/src/game/mod.rs
@@ -11,11 +11,12 @@ use crate::{gfx::DrawType, world::World};
 // very proof of concept-y
 #[derive(Default, Clone, Debug, PartialEq, Eq)]
 pub struct Game {
-	pub drawmap: World<DrawType>,
 	pub worlds: Worlds,
+
 	/// the uuid of the world we have open \
 	/// !!! can point to a world that doesn't exist so don't rely on that
 	pub main_id: WorldId,
+	pub drawmap: World<DrawType>,
 
 	/// moves of self.main
 	pub moves: IngameWorld,
@@ -54,6 +55,13 @@ impl Game {
 
 	pub fn switch_main(&mut self, id: WorldId) {
 		self.main_id = id;
+		if let Err(err) = self.regenerate_moves() {
+			eprintln!("error while regenerating moves after Game::switch_main call\n{err}")
+		}
+	}
+	pub fn regenerate_moves(&mut self) -> anyhow::Result<()> {
+		self.moves = IngameWorld::generate(self, self.main_id)?;
+		Ok(())
 	}
 
 	pub fn main(&self) -> Option<&World> {
