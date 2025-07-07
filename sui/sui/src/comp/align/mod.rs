2ba1620245a5b4e58236a9d88f9c59bca379a18f
manen <49951816+manen@users.noreply.github.com>
2025-07-07 14:34:39 +0200

    bye bye

--- sui/sui/src/comp/align/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod centered;
-pub use centered::Centered;
-
-pub mod at_end;
-pub use at_end::AtEnd;
--- sui/sui/src/comp/primitives/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod space;
-pub use space::{Margin, Space};
-
-pub mod text;
-pub use text::Text;
-
-pub mod color;
-pub use color::Color;
